c8c55c780d1223957ddfe40399cbb7cf7fcdbe02
dikson <diksonfer@gmail.com>
2021-09-27 13:05:47 -0300

    create start of trophy logic

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 trophyHolder = document.getElementById("tb");
+
+/*
 for (let i = 0; i < 10; i++) {
   trophyHolder.appendChild(randomTrophy());
 }
-displayChallsFromUser("diksown", "ct");
+*/
+
+getSolved("diksown").then((data) => {});
--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ $.extend($.fn.dataTable.defaults, {
   info: false,
 });
 
+// hardcoded, I know. but this is the only way
+//I could thought of getting them ordered.
+const categories = [
+  "Introduction",
+  "General",
+  "Mathematics",
+  "Symmetric Ciphers",
+  "RSA",
+  "Diffie-Hellman",
+  "Elliptic Curves",
+  "Hash Functions",
+  "Crypto on the Web",
+  "Misc",
+];
+
 // return a promise of info for a particular user
 async function getInfo(username) {
   let baseUrl = "https://cryptohack.org/api/user/";
@@ -65,18 +80,6 @@ function createChallNameLink(chall) {
 
 // generate a random chall. useful for testing.
 function randomChall() {
-  let categories = [
-    "Introduction",
-    "General",
-    "Mathematics",
-    "Symmetric Ciphers",
-    "RSA",
-    "Diffie-Hellman",
-    "Elliptic Curves",
-    "Hash Functions",
-    "Crypto on the Web",
-    "Misc",
-  ];
   let adjs = ["Portly", "Posh", "Positive", "Possible", "Potable"];
   let nouns = ["Paper", "Paperback", "Parade", "Parallelogram", "Parcel"];
   let name =
@@ -116,7 +119,8 @@ function displayChallList(challs, tableId) {
 async function displayUnsolvedFromUser(username, tableId) {
   // workaround for now, but correct 99.98% of the time
   let userAllChalls = "hellman";
-  let unsolvedChalls = await diffSolves(userAllChalls, username)[0];
+  let diffChalls = await diffSolves(userAllChalls, username);
+  let unsolvedChalls = diffChalls[0];
   displayChallList(unsolvedChalls, tableId);
 }
 
@@ -130,20 +134,31 @@ async function displayExclusiveChalls(user1, tableId1, user2, tableId2) {
 }
 
 // return a string with a trophy template.
-// the trophy2 and leftToSolve are optional,
-// if they aren't provided, only trophy1 is displayed.
-function infoToTrophy(category, trophy1, trophy2, leftToSolve) {
+// trophyInfo is either [trophy] or [trophy1, trophy2, leftToSolve]
+function infoToTrophy(category, trophyInfo) {
   let trophyTemplate;
-  if (trophy2 === undefined) {
+  let trophySuffix = {
+    e: "", // empty trophy
+    b: "-bronze", // bronze
+    s: "-silver", // silver
+    g: "-gold", // gold
+    r: "-golder", // golder
+  };
+
+  if (trophyInfo.length === 1) {
+    let trophy = trophyTemplate[0];
     trophyTemplate = `
-    <img src="/img/icons/trophy${trophy1}.svg" width="40" />`;
+    <img src="/img/icons/trophy${trophySuffix(trophy)}.svg" width="40" />`;
   } else {
+    let trophy1 = trophyTemplate[0];
+    let trophy2 = trophyTemplate[1];
+    let leftToSolve = trophyTemplate[2];
     trophyTemplate = `
-    <img src="/img/icons/trophy${trophy1}.svg" width="40" />
+    <img src="/img/icons/trophy${trophySuffix(trophy1)}.svg" width="40" />
       <div class="trophy-number">
         ${leftToSolve} 🡒
       </div>
-    <img src="/img/icons/trophy${trophy2}.svg" width="40" />`;
+    <img src="/img/icons/trophy${trophySuffix(trophy2)}.svg" width="40" />`;
   }
 
   let trophyCardTemplate = `
@@ -160,21 +175,10 @@ function infoToTrophy(category, trophy1, trophy2, leftToSolve) {
 
 // return a trophy DOM element with the given parameters
 // I think life would be easier if we used some js framework
-function trophyElement(category, t1, t2, leftToSolve) {
-  trophySuffix = {
-    e: "", // empty trophy
-    b: "-bronze", // bronze
-    s: "-silver", // silver
-    g: "-gold", // gold
-    r: "-golder", // golder
-  };
-
-  let t1Suf = trophySuffix[t1];
-  let t2Suf = trophySuffix[t2];
-
+function trophyElement(category, trophyInfo) {
   // string to element. best way I could find.
   let newNode = document.createElement("div");
-  newNode.innerHTML = infoToTrophy(category, t1Suf, t2Suf, leftToSolve);
+  newNode.innerHTML = infoToTrophy(category, trophyInfo);
   let trophyElementNode = newNode.firstElementChild;
   return trophyElementNode;
 }
@@ -182,21 +186,68 @@ function trophyElement(category, t1, t2, leftToSolve) {
 // generates a random trophy. useful for mocking.
 function randomTrophy() {
   let tList = ["e", "b", "s", "g", "r"];
-  let categories = [
-    "Introduction",
-    "General",
-    "Mathematics",
-    "Symmetric Ciphers",
-    "RSA",
-    "Diffie-Hellman",
-    "Elliptic Curves",
-    "Hash Functions",
-    "Crypto on the Web",
-    "Misc",
-  ];
   let t1 = tList[Math.floor(Math.random() * tList.length)];
   let t2 = tList[Math.floor(Math.random() * tList.length)];
   let toSolve = Math.floor(Math.random() * 100);
   let category = categories[Math.floor(Math.random() * categories.length)];
   return trophyElement(category, t1, t2, toSolve);
+}
+
+// return the number of trophies given the number of
+// challenges solved and the total number of challenges
+function currentTrophy(noSolved, totalChalls) {
+  let frac = noSolved / totalChalls;
+  if (frac < 0.25) return "e"; // empty trophy
+  if (frac < 0.5) return "b"; // bronze trophy
+  if (frac < 0.75) return "s"; // silver trophy
+  if (frac < 1.0) return "g"; // gold trophy
+  if (frac == 1.0) return "r"; // gold + star ('golder') trophy
+}
+
+// get the number of solved and the total number of challs
+// in the category. if all were solved, return a array only
+// with the star trophy. else, return a array with the current
+// trophy, the number of challs needed to go to the next trophy
+// and the next trophy.
+function trophyStats(noSolved, totalChalls) {
+  let curTrophy = currentTrophy(noSolved, totalChalls);
+  if (curTrophy === "r") return [curTrophy];
+  else {
+    for (let neededToSolve = 0; ; neededToSolve++) {
+      let nextTrophy = currentTrophy(noSolved + neededToSolve);
+      if (nextTrophy !== curTrophy) {
+        return [curTrophy, nextTrophy, neededToSolve];
+      }
+    }
+  }
+}
+
+// return a js object saying how many challs of each category were solved
+function solvedCount(challs) {
+  let categoryCount = {};
+  for (category in categories) {
+    categoryCount[category] = 0;
+  }
+  for (chall in challs) {
+    categoryCount[chall.category]++;
+  }
+  return categoryCount;
+}
+
+// return a trophy list with all trophies of a user.
+function trophyRoad(challs, allChalls) {
+  let categoryCount = solvedCount(challs);
+  let categoryTotal = solvedCount(allChalls);
+  let trophies = [];
+  for (category in categories) {
+    curTrophy = trophyStats(categoryCount[category], categoryTotal[category]);
+    curTrophyElement = trophyElement(category, curTrophy);
+    trophies.push(curTrophyElement);
+  }
+  return trophies;
+}
+
+function addTrophy(challs, tableId) {
+  let trophyHolder = document.getElementById(tableId);
+  trophyHolder.appendChild(randomTrophy());
 }
